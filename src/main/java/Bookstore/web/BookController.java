e0de55e42ae3a7743d82081221b3b6a360579063
huovelinjaakko <jaakkohuovelin@gmail.com>
2022-09-06 11:49:27 +0300

    Initial commit

---
+++ src/main/java/Bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("index")
+	public String mainPage() {
+		return "This is the main page";
+	}
+	
+}
